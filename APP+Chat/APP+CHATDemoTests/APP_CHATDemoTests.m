f301ef89b8a8e668ef7f776997184faddef46a8c
zhanglei <zhanglei1239@126.com>
2014-10-13 20:44:23 +0800

    test

---
+++ APP+Chat/APP+CHATDemoTests/APP_CHATDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  APP_CHATDemoTests.m
+//  APP+CHATDemoTests
+//
+//  Created by Mac mini on 14-10-9.
+//  Copyright (c) 2014年 zhanglei. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface APP_CHATDemoTests : XCTestCase
+
+@end
+
+@implementation APP_CHATDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
